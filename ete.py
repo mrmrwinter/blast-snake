b1f94a2dc3d519039b517a68f736bfc573a81237
Michael Winter <52489932+mrmrwinter@users.noreply.github.com>
2019-12-04 10:06:00 +0000

    ete with rooted
    ete with rooted

---
+++ ete.py
@@ -0,0 +1,22 @@
+from ete3 import Tree, TreeStyle, TextFace, faces, AttrFace, NodeStyle, PhyloTree
+
+    t = Tree(snakemake.input[0])
+
+    #### here to .....
+    #     ts = TreeStyle()
+    #     ts.mode = "c"
+    #     ts.show_leaf_name = True
+    #
+    #
+    #     ts.root_opening_factor = 1
+    #
+    #     ts.title.add_face(TextFace("If you can read this then TextFace works", fsize=20), column=0)
+    # get_farthest_oldest_node
+    ### ...here, is not working as it should. Read the documentation and figure out how it works cause ete seems pretty good once it's properly jammed through.
+
+
+	# midpoint root a tree
+    root_point = t.get_midpoint_outgroup()
+    t.set_outgroup(root_point)
+
+    t.render(snakemake.output[0])
